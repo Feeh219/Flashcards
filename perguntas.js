ca1ea4494b0d80e6dfc212a6f24c00790f80b69d
Feeh219 <168578282+Feeh219@users.noreply.github.com>
2024-11-06 10:25:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Naruto',
+    'Qual era o sonho de Jiraiya antes de morrer?',
+    'Ver um mundo de paz,sem guerras,realizado por uma criança da profecia'
+)
+
+criaCartao(
+    'Bungou stray dogs ',
+    'Qual é a habilidade de Osamu Dazai?',
+    'Eliminar outras habilidades'
+)
+
+criaCartao(
+    'Shingeki no Kyojin',
+    'Qual foi o segredo que Grisha Jaeger deixou para Eren no porão?',
+    'A existência de um mundo fora das muralhas e a verdade sobre Marley e Eldia'
+)
+
+criaCartao(
+    'Bleach',
+    'Quem foi o criador da Hogyoku,a pedra que dá poderes especiais a seres comuns?',
+    'Kisuke Urahara'
+)
